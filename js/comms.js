425c4a98aed7c4d9b640e37463b534a45a20def7
Gordon Williams <gw@pur3.co.uk>
2023-03-21 15:05:20 +0000

    Fix writing of timezone to storage if the timezone value is 0 (fix https://github.com/espruino/BangleApps/issues/2575)

--- js/comms.js
+++ js/comms.js
@@ -361,7 +361,7 @@ const Comms = {
       let cmd = '\x10setTime('+(d.getTime()/1000)+');';
       // in 1v93 we have timezones too
       cmd += 'E.setTimeZone('+tz+');';
-      cmd += "(s=>{s&&(s.timezone="+tz+")&&require('Storage').write('setting.json',s);})(require('Storage').readJSON('setting.json',1))\n";
+      cmd += "(s=>s&&(s.timezone="+tz+",require('Storage').write('setting.json',s)))(require('Storage').readJSON('setting.json',1))\n";
       Comms.write(cmd);
     });
   },
