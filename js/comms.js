3ff983df842443c0cd157bfa5bf15b12ec5caf98
RKBoss6 <kabironkar@gmail.com>
2026-01-21 16:09:14 -0500

    Fix message formatting for firmware update notification

--- js/comms.js
+++ js/comms.js
@@ -324,7 +324,7 @@ const Comms = {
         function doUpload() {
           Progress.show({min:0.05, max:0.10}); // 5-10% for progress writing
           console.log(JSON.stringify(app));
-          let msg = app.id=="fwupdate"?`Downloading firmware...`:`Installing\n'${app.name}'...`
+          let msg = app.id=="fwupdate"?`Downloading\nfirmware...`:`Installing\n'${app.name}'...`
           Comms.showMessage(msg).
             then(() => Comms.write("\x10"+Comms.getProgressCmd()+"\n", {noWait:true})).
             then(() => {
